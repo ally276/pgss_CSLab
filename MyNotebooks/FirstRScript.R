f7a35e232260b9fb0c2e26f1f94ad49b3768a776
ally276 <67803806+ally276@users.noreply.github.com>
2020-07-03 17:17:19 -0400

    Update FirstRScript.R

--- MyNotebooks/FirstRScript.R
+++ MyNotebooks/FirstRScript.R
@@ -1,2 +1,6 @@
-a<-1
-print(a)
+#https://raw.githubusercontent.com/menonpg/COVID19_Chicago/master/timeSeries_COVID19_Chicago.csv
+
+myData<- read.csv("https://raw.githubusercontent.com/menonpg/COVID19_Chicago/master/timeSeries_COVID19_Chicago.csv")
+
+myData$Date <- as.Date(myData$Date, format="%Y-%m-%d")
+print(summary(myData))
